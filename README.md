6389001d8bec46cca79f6cb945f111df5c6d2cbe
pyshreyash <business.coder@gmail.com>
2025-05-23 01:31:17 +0530

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AdaptiveUGC
+This is the repositiory for the paper -----
+to run the file do this
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import networkx as nx
 import torch_geometric
 from scatter_letters import sl
 
+#Hello ji
 import seaborn as sns
 from sklearn.manifold import TSNE
 
